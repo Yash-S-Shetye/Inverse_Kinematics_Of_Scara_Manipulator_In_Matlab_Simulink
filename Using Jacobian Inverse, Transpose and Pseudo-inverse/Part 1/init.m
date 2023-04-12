b6dfe48854507a7f79712a9e71477ab8db9d2275
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-04-12 11:29:20 -0400

    Update init.m

--- Using Jacobian Inverse, Transpose and Pseudo-inverse/Part 1/init.m
+++ Using Jacobian Inverse, Transpose and Pseudo-inverse/Part 1/init.m
@@ -4,7 +4,10 @@ clc
 vrclear
 vrclose
 
-load('kinematic_traj.mat');
+load('../kinematic_traj.mat');
 clik_inverse;
 sim('clik_inverse.mdl', t);
-SCARA_VR_VISUALIZE(squeeze(q(:,1,:)), false);
+clik_transpose;
+sim('clik_transpose.slx', t);
+%addpath('C:\Users\DELL\Desktop\Masters Studies\Foundation of Robotics\project1\student\code\visualization')
+%SCARA_VR_VISUALIZE(squeeze(q(:,1,:)), true);
